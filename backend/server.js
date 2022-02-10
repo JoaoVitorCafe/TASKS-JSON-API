d28e460a9f74e668e25ad5da2a26e89e10f33aa0
João Vítor Café <batistajv2012@gmail.com>
2022-02-10 20:13:02 -0300

    update 0.2

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const taskRoute = require('./routes/taskRoute')
 
 // Create JSON file
 // Implement status on the tasks
 
-
 const port = process.env.PORT || 3000
 
 const app = express()
 
+app.use(cors())
 app.use(express.urlencoded({extended : false}))
 app.use(express.json())
 app.use(morgan('dev'))
---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+// async function getTasks(url) {
+//         const res = await fetch(url)
+//         const data = await res.json()
+
+//         console.log(data)
+// }
+
+// getTasks('http://localhost:3000/api/tasks/')
+
+
+// fetch('http://localhost:3000/api/tasks/')
+//     .then(response => response.json())
+//     .then(data => console.log(data))
